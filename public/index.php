2041549e9325836556e9eb33a95d505aa1f9b915
guoguo <13120230872@163.com>
2019-12-07 12:25:57 +0800

    0001

--- public/index.php
+++ public/index.php
@@ -5,19 +5,18 @@ require '../vendor/autoload.php';
 $start 	= new Index();
 $start->run();
 
-/*spl_autoload_register('my_library');
+spl_autoload_register('my_library');
 function my_library($class) {
 	if ($class) {
     	$file = str_replace("\\", "/", $class);
 
     	$file = $file.'.php';
-    	
+
     	if (file_exists($file)) {
     		require_once $file;
     	}
     }
 }
-*/
 
 /*$mono 	= new Courier("Monospace");
 echo $mono->name;
